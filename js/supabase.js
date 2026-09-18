a11f7f9cf3fb2ef06bedda7780f12c260947c5c6
Titik Koma Coffee <titikkomacoffee01@gmail.com>
2026-09-18 22:14:33 +0800

    Delete js directory

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-// GANTI dengan URL & KEY milik Anda dari Supabase Settings → API
-const SUPABASE_URL = 'https://dcvuzxuxwmcowcnaevgp.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjdnV6eHV4d21jb3djbmFldmdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MjcyNDIsImV4cCI6MjEwNTMwMzI0Mn0.svdPVapnSZ4fHTmO2mAt9UUmJKYxJM1Kuie9cjBg3OI';
-
-// Pakai CDN supabase (dimuat di HTML)
-const { createClient } = supabase;
-const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
