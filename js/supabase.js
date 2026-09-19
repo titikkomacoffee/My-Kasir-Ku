6a65f2b5c36ac38f52384b346e020bc8fd328ba0
Titik Koma Coffee <titikkomacoffee01@gmail.com>
2026-09-19 08:26:27 +0800

    Update Supabase URL and anon key
    Updated Supabase connection URL and anon key.

--- js/supabase.js
+++ js/supabase.js
@@ -3,8 +3,8 @@
 // GANTI dengan URL & KEY milik Anda!
 // ============================================
 
-const SUPABASE_URL = 'https://xxxxx.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+const SUPABASE_URL = 'https://dcvuzxuxwmcowcnaevgp.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjdnV6eHV4d21jb3djbmFldmdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MjcyNDIsImV4cCI6MjEwNTMwMzI0Mn0.svdPVapnSZ4fHTmO2mAt9UUmJKYxJM1Kuie9cjBg3OI';
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
